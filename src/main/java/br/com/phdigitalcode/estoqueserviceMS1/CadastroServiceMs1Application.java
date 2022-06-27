362a6f9d18a8fbbe9367fa7750511e266bec3435
Phelipp Nascimento <phelippnd@gmail.com>
2022-06-26 23:38:09 -0300

    primeiro commit

---
+++ src/main/java/br/com/phdigitalcode/estoqueserviceMS1/CadastroServiceMs1Application.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.estoqueserviceMS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroServiceMs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroServiceMs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phdigitalcode/cadastroserviceMS1/CadastroServiceMs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.cadastroserviceMS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroServiceMs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
